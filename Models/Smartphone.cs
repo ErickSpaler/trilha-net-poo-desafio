614f4644fd9bbf47b74170107a64610a8a115aae
erick <erickspaler@hotmail.com>
2025-07-08 11:40:38 -0300

    Desafio concluido

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -1,15 +1,83 @@
+using System.Linq;
 namespace DesafioPOO.Models
 {
     public abstract class Smartphone
     {
-        public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        protected string numero;
+        protected string modelo;
+        protected string imei;
+        protected int memoria;
 
-        public Smartphone(string numero)
+        public string Numero
+        {
+            get => numero;
+
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("O número não pode ser vazio.");
+                }
+                numero = value;
+            }
+        }
+
+        protected string Modelo
+        {
+            get => modelo.ToUpper();
+
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("O modelo não pode ser vazio.");
+                }
+                modelo = value;
+            }
+        }
+
+        protected string IMEI
+        {
+            get => imei;
+
+            set
+            {
+                if (value.All(char.IsDigit))
+                {
+                    imei = value;
+                }
+                else
+                {
+                    throw new ArgumentException("O campo IMEI só deve receber números.");
+                }
+            }
+        }
+
+        protected int Memoria
+        {
+            get => memoria;
+
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("A memória não pode ser vazia.");
+                }
+                memoria = value;
+            }
+        }
+
+
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            IMEI = imei;
+            Memoria = memoria;
         }
+
+
+
 
         public void Ligar()
         {
--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
 ﻿using DesafioPOO.Models;
+using System;
+using System.Linq;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+
+
+Smartphone nokia = new Nokia("11999999999", "3310", "123456789012345", 64);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine();
+
+Smartphone iphone = new Iphone("11988888888", "14 Pro", "987654321098765", 128);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Instagram");
